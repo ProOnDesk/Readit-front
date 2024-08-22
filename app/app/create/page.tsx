edc3c517229a57a2911b1297f03148a503c729fd
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-08-22 18:06:03 +0200

    change title and page description (/create) and layout delete h-screem div

--- app/app/create/page.tsx
+++ app/app/create/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+	title: 'Stwórz artykuł | Just Read',
+	description: 'Place where u can read',
+};
 
 export default function Page() {
-  return (
-    <div>Kreator</div>
-  )
+	return <div>Kreator</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,48 @@
-import type { Metadata } from "next";
-import { Jost } from "next/font/google";
-import "./globals.css";
-import "@mantine/nprogress/styles.css";
-import "@mantine/core/styles.css";
-import AppProvider from "./_components/AppProvider";
-import Navigation from "./_components/nav/Navigation";
-import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
-import { RouterTransition } from "./_components/nav/RouterTransition";
+import type { Metadata } from 'next';
+import { Jost } from 'next/font/google';
+import './globals.css';
+import '@mantine/nprogress/styles.css';
+import '@mantine/core/styles.css';
+import AppProvider from './_components/AppProvider';
+import Navigation from './_components/nav/Navigation';
+import { ColorSchemeScript, createTheme, MantineProvider } from '@mantine/core';
+import { RouterTransition } from './_components/nav/RouterTransition';
 
 const jost = Jost({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "ReadIt | Just Read",
-  description: "Place where u can read",
+	title: 'ReadIt | Just Read',
+	description: 'Place where u can read',
 };
 
 // const theme = createTheme({});
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      {/* <AppProvider> */}
-      <body className={jost.className}>
-        <MantineProvider>
-          <div
-            id="app"
-            className="relative font-display flex flex-col items-center justify-center w-full h-full overflow-x-hidden"
-          >
-            <Navigation />
-            <div className="pt-16 md:pt-20">
-              <RouterTransition />
-              {children}
-
-              {/* do usuniecia */}
-              <div className="h-screen"></div>
-            </div>
-          </div>
-        </MantineProvider>
-      </body>
-      {/* </AppProvider> */}
-    </html>
-  );
+	return (
+		<html lang='en'>
+			{/* <AppProvider> */}
+			<body className={jost.className}>
+				<MantineProvider>
+					<div
+						id='app'
+						className='relative font-display flex flex-col items-center justify-center w-full h-full overflow-x-hidden'
+					>
+						<Navigation />
+						<div className='pt-16 md:pt-20'>
+							<RouterTransition />
+							{children}
+						</div>
+					</div>
+				</MantineProvider>
+			</body>
+			{/* </AppProvider> */}
+		</html>
+	);
 }
