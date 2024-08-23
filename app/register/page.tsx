8e2a4e377f105c8671391617163e61a22c1d4e4b
Lukasz <7lukasz8@gmail.com>
2024-08-23 18:26:21 +0200

    register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "../_components/account/RegisterForm";
 
 export default function Page() {
   return (
-    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] w-full flex justify-center items-center px-4">
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] w-full flex justify-center items-center px-4 sm400:px-10 sm500:px-20 sm:bg-whiteSecond">
       <RegisterForm />
     </div>
   );
