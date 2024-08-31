544960cf20b967a6c8512dd48114a0e0956b2889
Lukasz <7lukasz8@gmail.com>
2024-08-31 13:44:19 +0200

    section with users - top follows

--- app/_components/creatorsBrowse/CreatorsSecion.tsx
+++ app/_components/creatorsBrowse/CreatorsSecion.tsx
@@ -7,23 +7,35 @@ import {
 } from "@/app/_redux/features/creatorsApiSlice";
 import React, { useEffect, useState } from "react";
 import UserItem from "./UserItem";
+import { FiChevronsDown } from "react-icons/fi";
+import Spinner from "../ui/Spinner";
 
 export default function CreatorsSecion() {
   const [topUsersFollows, setTopUsersFollows] = useState<User[]>([]);
   const [page, setPage] = useState(2);
-  const [prevPage, setPrevPage] = useState<number | null>(null);
   const [getUsersTopFollows, { isLoading, data }] =
     useGetUsersTopFollowsMutation();
+  // const [userWidth, setUserWidth] = useState(null);
+  const [itemsPerPage, setItemsPerPage] = useState(2);
+
+  useEffect(function () {
+    setItemsPerPage(
+      (window.innerWidth > 1280 && 8) ||
+        (window.innerWidth > 1024 && 6) ||
+        (window.innerWidth > 640 && 4) ||
+        2
+    );
+  }, []);
 
   useEffect(
     function () {
-      getUsersTopFollows({ page: 1 })
+      getUsersTopFollows({ page: 1, size: 12 })
         .unwrap()
         .then((res: PaginatonType) => {
           setTopUsersFollows(res.items);
         });
     },
-    [getUsersTopFollows]
+    [getUsersTopFollows, itemsPerPage]
   );
 
   useEffect(
@@ -36,23 +48,53 @@ export default function CreatorsSecion() {
   );
 
   return (
-    <div className="w-full px-4 ">
+    <div className="w-full px-4 pb-32">
       <h3 className="font-semibold text-2xl sm500:text-3xl sm:text-4xl text-center">
         Najchętniej obserwowani
       </h3>
-      <div className="w-full mx-auto">
-        <div className="w-full grid sm:grid-cols-2 gap-4">
+      <div className="w-full mx-auto pt-16 flex flex-col justify-center items-center gap-10">
+        <div className="w-full h-fit grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center mx-auto justify-center max-w-[680px] lg:max-w-[1020px] xl:max-w-[1360px] transition-all duration-300">
           {topUsersFollows.map((user) => (
             <UserItem user={user} key={user.id} />
           ))}
-          <button
-            onClick={() => {
-              setPage((page) => page + 1);
-              getUsersTopFollows({ page });
-            }}
-          >
-            Load more
-          </button>
+        </div>
+        <div className=" flex justify-center items-center gap-5">
+          {data && data.page > 1 && (
+            <button
+              className="px-8 py-3  text-mainGreen rounded-full transition-all duration-300 hover:border-mainGreenSecond flex justify-center items-center gap-2 text-lg font-semibold z-10 border-2 border-mainGreen"
+              onClick={() => {
+                setPage(2);
+                getUsersTopFollows({ page: 1, size: 12 })
+                  .unwrap()
+                  .then((res: PaginatonType) => {
+                    setTopUsersFollows(res.items);
+                  });
+              }}
+            >
+              Schowaj wszystko
+            </button>
+          )}
+          {data && data.pages > data.page && (
+            <button
+              className="h-[52px] w-[210px] bg-mainGreen text-white rounded-full transition-all duration-300 hover:bg-mainGreenSecond flex justify-center items-center gap-2 text-lg font-semibold z-10"
+              onClick={() => {
+                setPage((page) => page + 1);
+                getUsersTopFollows({ page, size: itemsPerPage });
+              }}
+            >
+              {isLoading ? (
+                <Spinner />
+              ) : (
+                <>
+                  <span>
+                    <FiChevronsDown size={24} />
+                  </span>
+                  Pokaż więcej
+                </>
+              )}
+            </button>
+          )}
+          {isLoading && <Spinner color="green" />}
         </div>
       </div>
     </div>
