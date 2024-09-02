af19a2eb396935267da5604840ef1411ec9ed0ab
Lukasz <7lukasz8@gmail.com>
2024-09-02 22:31:58 +0200

    fix for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
 import AboutReadit from "./_components/homepage/AboutReadit";
 import ExplanationAboutReadit from "./_components/homepage/ExplanationAboutReadit";
 import HeaderHomepage from "./_components/homepage/HeaderHomepage";
-import JoinButton from "./_components/homepage/JoinBtn";
-import WelcomeImg from "./_components/homepage/WelcomeImg";
-import WelcomeText from "./_components/homepage/WelcomeText";
 
 export default function Home() {
-	return (
-		<div className="mx-auto ">
-			<HeaderHomepage />
-			<AboutReadit />
-			<ExplanationAboutReadit />
-			{/* <
+  return (
+    <div className="mx-auto ">
+      <HeaderHomepage />
+      <AboutReadit />
+      <ExplanationAboutReadit />
+      {/* <
 			<PopularArticlesHomepage />
 			<FooterHomepage />
 			<Footer /> */}
-		</div>
-	);
+    </div>
+  );
 }
