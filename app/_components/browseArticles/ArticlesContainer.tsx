c4b5c3c394a48942cad092d843942eb9c0499649
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-09-03 16:55:08 +0200

    loading content works

--- app/_components/browseArticles/ArticlesContainer.tsx
+++ app/_components/browseArticles/ArticlesContainer.tsx
@@ -1,27 +1,32 @@
-import React, { Suspense } from "react";
-import ArticleItem from "../profile/ArticleItem";
-import { Article } from "@/app/_redux/features/authApiSlice";
-import Spinner from "../ui/Spinner";
-import { Pagination } from "@mui/material";
-import { PaginationTypeArticles } from "@/app/_redux/features/articlesApiSlice";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import PaginationArticles from "./PaginationArticles";
+import React, { Suspense } from 'react';
+import ArticleItem from '../profile/ArticleItem';
+import { Article } from '@/app/_redux/features/authApiSlice';
+import Spinner from '../ui/Spinner';
+import { Pagination } from '@mui/material';
+import { PaginationTypeArticles } from '@/app/_redux/features/articlesApiSlice';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import PaginationArticles from './PaginationArticles';
+import { getArticlesSearch } from '@/app/_actions/articlesActions';
+import { unstable_noStore as noStore } from 'next/cache';
+import { SearchParams } from '@/app/browse/page';
 
 interface ArticlesContainerProps {
-  data: PaginationTypeArticles;
+	pa: SearchParams;
 }
 
 export default async function ArticlesContainer({
-  data,
+	pa,
 }: ArticlesContainerProps) {
-  return (
-    <>
-      <div className="grid grid-cols-1 sm550:grid-cols-2 md800:grid-cols-3 lg1100:grid-cols-4 gap-x-3 gap-y-6 pb-10">
-        {data?.items.map((article) => (
-          <ArticleItem article={article} key={article.id} />
-        ))}
-      </div>
-      <PaginationArticles data={data} />
-    </>
-  );
+	noStore();
+	const data = await getArticlesSearch(pa);
+	return (
+		<>
+			<div className='grid grid-cols-1 sm550:grid-cols-2 md800:grid-cols-3 lg1100:grid-cols-4 gap-x-3 gap-y-6 pb-10'>
+				{data?.items.map((article) => (
+					<ArticleItem article={article} key={article.id} />
+				))}
+			</div>
+			<PaginationArticles data={data} />
+		</>
+	);
 }
--- app/_components/browseArticles/Content.tsx
+++ app/_components/browseArticles/Content.tsx
@@ -1,45 +1,26 @@
-"use server";
-
-import React, { lazy, Suspense, useEffect, useState } from "react";
-import SelectFilter from "./SelectFilter";
-import { useDebounce } from "@/app/_hooks/useDebounce";
-import Filters from "./Filters";
+import React, { lazy, Suspense, useEffect, useState } from 'react';
+import SelectFilter from './SelectFilter';
+import { useDebounce } from '@/app/_hooks/useDebounce';
+import Filters from './Filters';
 import {
-  PaginationTypeArticles,
-  useGetArticlesSearchQuery,
-} from "@/app/_redux/features/articlesApiSlice";
-import ArticleItem from "../profile/ArticleItem";
-import Spinner from "../ui/Spinner";
-const LazyComponentWithData = lazy(() => import("./ArticlesContainer"));
+	PaginationTypeArticles,
+	useGetArticlesSearchQuery,
+} from '@/app/_redux/features/articlesApiSlice';
+import ArticleItem from '../profile/ArticleItem';
+import Spinner from '../ui/Spinner';
+
 // import ArticlesContainer from "./ArticlesContainer";
-import { SearchParams } from "@/app/browse/page";
-import { getArticlesSearch } from "@/app/_actions/articlesActions";
+import { SearchParams } from '@/app/browse/page';
+import { getArticlesSearch } from '@/app/_actions/articlesActions';
+import ArticlesContainer from './ArticlesContainer';
 
 interface ContentProps {
-  params: SearchParams;
+	params: SearchParams;
+	pa: any;
 }
 
 // export const revalidate = 0;
 
-export default async function Content({ params }: ContentProps) {
-  const data: PaginationTypeArticles = await getArticlesSearch(params);
-
-  return (
-    <div className="w-full max-w-[1800px] mx-auto">
-      <SelectFilter materialsCount={data.total} />
-      <div className="grid md:grid-cols-[320px_1fr]">
-        <Filters />
-        <Suspense
-          fallback={
-            <div className="grid place-items-center">
-              <Spinner color="green" size="big" />
-            </div>
-          }
-          key={data.items.length}
-        >
-          <LazyComponentWithData data={data} />
-        </Suspense>
-      </div>
-    </div>
-  );
+export default async function Content({ params, pa }: ContentProps) {
+	return <div></div>;
 }
