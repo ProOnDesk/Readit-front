bf1306aeb8e77e3b64964c666659567d3b3d8f84
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-09-03 13:55:28 +0200

    new logo

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Logo() {
 	return (
 		<Link href='/' className='mr-5 h-full py-[1.4rem]'>
-			<img src='/pencilLogo.png' alt='logo' className='h-full' />
+			<img src='/ReadIt-logo.svg' alt='logo' className='h-full' />
 		</Link>
 	);
 }
