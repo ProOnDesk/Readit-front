66e16df0985a14269a98fa8b8fd84383ab0ed20b
Lukasz <7lukasz8@gmail.com>
2024-10-05 17:11:34 +0200

    articles pagination for creator page

--- app/_components/creatorsPage/ProfileArticlesCreator.tsx
+++ app/_components/creatorsPage/ProfileArticlesCreator.tsx
@@ -1,11 +1,28 @@
+'use client'
+
 import { User } from "@/app/_redux/features/authApiSlice";
 import ArticleItem from "../profile/ArticleItem";
+import { useSearchParams } from "next/navigation";
+import { useGetUserArticlesQuery } from "@/app/_redux/features/articlesApiSlice";
+import { useEffect } from "react";
+import PaginationArticles from "../browseArticles/PaginationArticles";
+import Spinner from "../ui/Spinner";
 
 interface ProfileArticlesProps {
   user: User | undefined;
 }
 
 export default function ProfileArticlesCreator({ user }: ProfileArticlesProps) {
+  const searchParams = useSearchParams();
+  const { data, refetch, isLoading, isFetching } = useGetUserArticlesQuery({
+    userId: user?.id || 0,
+    page: Number(searchParams.get("page") || 1),
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [searchParams, refetch]);
+
   return (
     <div className="px-8 mt-10 sm:px-12 lg:px-20 lg1200:px-12 max-w-[1200px] mx-auto bg-white md900:mt-0 md900:py-8 lg1200:py-10 lg1200:mt-4 lg1200:rounded-2xl lg1200:shadow-lg ">
       <div className="flex w-full justify-between items-start flex-col sm500:items-center sm500:flex-row gap-3">
@@ -25,10 +42,21 @@ export default function ProfileArticlesCreator({ user }: ProfileArticlesProps) {
             <p>Twórca nie utworzył żadnych materiałów</p>
           </div>
         )}
-        <div className="grid grid-cols-1 sm550:grid-cols-2 md800:grid-cols-3 lg1100:grid-cols-4 gap-x-3 gap-y-6 pb-10">
-          {user?.articles.map((article, i) => (
-            <ArticleItem article={article} key={i} />
-          ))}
+        <div className="grid grid-cols-1 sm550:grid-cols-2 md800:grid-cols-3 lg1100:grid-cols-4 gap-x-3 gap-y-6">
+          {isLoading || isFetching ? (
+            <div className="col-span-1 sm550:col-span-2 md800:col-span-3 lg1100:col-span-4 h-[296.5px] flex justify-center items-center">
+              <Spinner color="green" size="big" />
+            </div>
+          ) : (
+            data?.items.map((article, i) => (
+              <ArticleItem article={article} key={i} isCreator />
+            ))
+          )}
+          {data?.pages && data.pages > 1 && (
+            <div className="col-span-1 sm550:col-span-2 md800:col-span-3 lg1100:col-span-4">
+              <PaginationArticles data={data!} />
+            </div>
+          )}
         </div>
       </div>
     </div>
