06a44a1141d20619c1f37ecbdbc6ab285eeb0d40
Lukasz <7lukasz8@gmail.com>
2024-10-06 21:38:16 +0200

    get user articles action

--- app/_actions/profileActions.ts
+++ app/_actions/profileActions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { PaginationTypeArticles } from "../_redux/features/articlesApiSlice";
 import { User } from "../_redux/features/authApiSlice";
 
 export async function fetchUserByIdQuery({ id }: { id: string }) {
@@ -20,5 +21,53 @@ export async function fetchUserByIdQuery({ id }: { id: string }) {
     return data;
   } catch (error) {
     console.log("Nie udało się pobrać artykułu", error);
+  }
+}
+
+export async function fetchUserArticles({
+  userId,
+  page,
+}: {
+  userId: number;
+  page: string;
+}) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST}/user/get/articles/${userId}?page=${page}&size=12`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`Błąd serwera: ${response.status}`);
+    }
+    const data: PaginationTypeArticles = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Nie udało się pobrać artykułów", error);
+  }
+}
+
+export async function fetchUsersNumber() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST}/user/followers/top/?page=1&size=1`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`Błąd serwera: ${response.status}`);
+    }
+    const data: PaginationTypeArticles = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Nie udało się pobrać liczby uzytkownikow", error);
   }
 }
