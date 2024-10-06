97bffdf9aa7b40402e5840fd6a626e01ec6d26bd
Lukasz <7lukasz8@gmail.com>
2024-10-06 21:38:56 +0200

    profile articles pagination

--- app/_components/creatorsPage/ProfileArticlesCreator.tsx
+++ app/_components/creatorsPage/ProfileArticlesCreator.tsx
@@ -1,27 +1,22 @@
-'use client'
-
 import { User } from "@/app/_redux/features/authApiSlice";
 import ArticleItem from "../profile/ArticleItem";
 import { useSearchParams } from "next/navigation";
-import { useGetUserArticlesQuery } from "@/app/_redux/features/articlesApiSlice";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import PaginationArticles from "../browseArticles/PaginationArticles";
 import Spinner from "../ui/Spinner";
+import { fetchUserArticles } from "@/app/_actions/profileActions";
+import ArticlesContainerCreator from "./ArticlesContainerCreator";
 
 interface ProfileArticlesProps {
-  user: User | undefined;
+  user: User;
+  page: string;
 }
 
-export default function ProfileArticlesCreator({ user }: ProfileArticlesProps) {
-  const searchParams = useSearchParams();
-  const { data, refetch, isLoading, isFetching } = useGetUserArticlesQuery({
-    userId: user?.id || 0,
-    page: Number(searchParams.get("page") || 1),
-  });
-
-  useEffect(() => {
-    refetch();
-  }, [searchParams, refetch]);
+export default async function ProfileArticlesCreator({
+  user,
+  page,
+}: ProfileArticlesProps) {
+  const data = await fetchUserArticles({ userId: user.id, page });
 
   return (
     <div className="px-8 mt-10 sm:px-12 lg:px-20 lg1200:px-12 max-w-[1200px] mx-auto bg-white md900:mt-0 md900:py-8 lg1200:py-10 lg1200:mt-4 lg1200:rounded-2xl lg1200:shadow-lg ">
@@ -43,15 +38,17 @@ export default function ProfileArticlesCreator({ user }: ProfileArticlesProps) {
           </div>
         )}
         <div className="grid grid-cols-1 sm550:grid-cols-2 md800:grid-cols-3 lg1100:grid-cols-4 gap-x-3 gap-y-6">
-          {isLoading || isFetching ? (
-            <div className="col-span-1 sm550:col-span-2 md800:col-span-3 lg1100:col-span-4 h-[296.5px] flex justify-center items-center">
-              <Spinner color="green" size="big" />
-            </div>
-          ) : (
-            data?.items.map((article, i) => (
-              <ArticleItem article={article} key={i} isCreator />
-            ))
-          )}
+          <Suspense
+            key={user.id + page}
+            fallback={
+              <div className="col-span-1 sm550:col-span-2 md800:col-span-3 lg1100:col-span-4 h-[296.5px] flex justify-center items-center">
+                <Spinner color="green" size="big" />
+              </div>
+            }
+          >
+            <ArticlesContainerCreator page={page} userId={user.id} />
+          </Suspense>
+
           {data?.pages && data.pages > 1 && (
             <div className="col-span-1 sm550:col-span-2 md800:col-span-3 lg1100:col-span-4">
               <PaginationArticles data={data!} />
