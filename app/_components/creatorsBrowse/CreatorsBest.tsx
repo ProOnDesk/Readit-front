2aca5fe6871e546034483cac78a55b78ad4c5018
Lukasz <7lukasz8@gmail.com>
2024-10-15 22:47:13 +0200

    dummy commit

--- app/_components/creatorsBrowse/CreatorsBest.tsx
+++ app/_components/creatorsBrowse/CreatorsBest.tsx
@@ -5,7 +5,7 @@ export default function CreatorsBest() {
     <div className="w-full flex justify-center items-center text-center py-20 md:py-48 md:justify-end px-6 md:px-14 italic sm500:text-lg md:text-xl max-w-[1800px] mx-auto">
       <div className=" flex justify-center items-center gap-10 h-full">
         <p className="max-w-[650px] md:text-right">
-         
+         cos tu napisze
         </p>
         <div className="w-1 h-[180px] bg-mainGreen hidden md:block" />
       </div>
