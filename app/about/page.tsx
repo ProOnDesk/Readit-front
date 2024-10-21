1202a97b69cc183da32a7f12432e019ed2b16020
Lukasz <7lukasz8@gmail.com>
2024-10-21 22:55:33 +0200

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
     <>
       <div className="md:min-h-[calc(100vh-80px)] min-h-[calc(100vh-64px)] w-full flex justify-center items-start  lg1200:bg-whiteSecond">
         <div className="px-8 w-full h-full mt-10 sm:px-12 lg:px-20 lg1200:px-12 max-w-[1200px] mx-auto bg-white md900:mt-12 md900:py-8 lg1200:py-10 lg1200:mt-12 lg1200:rounded-2xl lg1200:shadow-lg">
-          <h4 className="text-2xl font-medium">Regulamin Readit</h4>
+          <h4 className="text-2xl font-medium">O zespole tworzącym Readit</h4>
           <div className="py-4 text-left flex gap-2 group">
-            <p>Regulamin kiedyś zostanie napisany...</p>
+            <p>Kiedyś ta strona zostanie zrobiona...</p>
           </div>
         </div>
       </div>
