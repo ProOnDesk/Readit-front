db1e65259152994fc3e1a6e4eb95ff6a2d0f2084
Lukasz <7lukasz8@gmail.com>
2024-11-02 21:16:38 +0100

    new api slice for collections

---
+++ app/_redux/features/collectionsApiSlice.ts
@@ -0,0 +1,97 @@
+import { FieldValues } from "react-hook-form";
+import { apiSlice } from "../services/apiSlice";
+import { Article } from "./authApiSlice";
+
+export interface Collection {
+  id: number;
+  owner_id: number;
+  title: string;
+  short_description: string;
+  discount_percentage: number;
+  collection_image_url: string;
+  price: number;
+  created_at: string;
+  updated_at: string;
+  articles_count: number;
+  rating: number;
+  articles_id: number[];
+}
+
+export interface CollectionsPagination {
+  items: Collection[];
+  total: number;
+  pages: number;
+  page: number;
+  size: number;
+}
+
+export interface CollectionWithDetails extends Collection {
+  articles: Article[];
+}
+
+const collectionsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createCollection: builder.mutation({
+      query: ({ data, file }) => {
+        const formData = new FormData();
+        console.log(data);
+        formData.append("collection", JSON.stringify(data));
+
+        formData.append("collection_image", file);
+
+        return {
+          url: `/articles/collection`,
+          method: "POST",
+          body: formData,
+        };
+      },
+    }),
+    getMyCollections: builder.query<CollectionsPagination, { page: string }>({
+      query: ({ page }) => ({
+        url: `/articles/collections/me?page=${page}&size=12`,
+        method: "GET",
+      }),
+    }),
+    getCollectionById: builder.mutation<
+      CollectionWithDetails,
+      { collection_id: number }
+    >({
+      query: ({ collection_id }) => ({
+        url: `/articles/collection/detail/${collection_id}`,
+        method: "GET",
+      }),
+    }),
+    editCollectionById: builder.mutation<
+      Collection,
+      { data: FieldValues; file: File; collection_id: number }
+    >({
+      query: ({ data, file, collection_id }) => {
+        const formData = new FormData();
+        console.log(data);
+        formData.append("collection", JSON.stringify(data));
+
+        formData.append("collection_image", file);
+
+        return {
+          url: `/articles/collection/${collection_id}`,
+          method: "PATCH",
+          body: formData,
+        };
+      },
+    }),
+    deleteCollectionById: builder.mutation<void, { collection_id: number }>({
+      query: ({ collection_id }) => ({
+        url: `/articles/collection/${collection_id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateCollectionMutation,
+  useGetMyCollectionsQuery,
+  useGetCollectionByIdMutation,
+  useEditCollectionByIdMutation,
+  useDeleteCollectionByIdMutation,
+} = collectionsApiSlice;
